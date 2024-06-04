18a556965cd059c2829e8703acdbd68ddd1ce993
Sergine Winner <serginew1509@gmail.com>
2024-06-04 17:42:02 +0700

    finsih

--- js/script.js
+++ js/script.js
@@ -143,7 +143,26 @@ learnMoreButton.addEventListener('click', function () {
 )
 
 
+// SLIDER
+let slideIndex = 0;
+showSlides();
 
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlide");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" ", " active");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000);
+}
 
 
 
